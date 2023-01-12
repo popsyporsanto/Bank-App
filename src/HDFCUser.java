e132ad44bc6b8691b41bc9621914bed265c7684c
popson porsanto <pops7onpors@gmail.com>
2023-01-12 17:13:18 +0530

    Add Bank Application project

--- src/HDFCUser.java
+++ src/HDFCUser.java
@@ -1,21 +1,101 @@
+import java.util.Objects;
+import java.util.UUID;
+
 public class HDFCUser implements BankInterface{
+
+    private String name;
+    private String accountNo;
+    private double balance;
+    private  String password;
+    private static double rateOfInterest;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static double getRateOfInterest() {
+        return rateOfInterest;
+    }
+
+    public static void setRateOfInterest(double rateOfInterest) {
+        HDFCUser .rateOfInterest = rateOfInterest;
+    }
+
+    public HDFCUser (String name, double balance, String password) {
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+
+        //bank is initiaizing
+        this.rateOfInterest=6.5;
+        this.accountNo=String.valueOf(UUID.randomUUID());
+
+    }
+
+
+
     @Override
     public double checkBalance() {
-        return 0;
+
+        return balance;
     }
 
     @Override
     public String addMoney(int amount) {
-        return null;
+        balance=balance+amount;
+        return "Your new balance is:"+balance;
     }
 
     @Override
-    public String withdrawMoney(int amount, String password) {
-        return null;
+    public String withdrawMoney(int amount, String enteredPassword)
+    {
+
+        if(Objects.equals(enteredPassword,password)){
+            if(amount>balance){
+                return "Sorry ! Insufficient Money";
+            }
+            else{
+                balance=balance-amount;
+                return "Lots of money";
+            }
+        }
+        else{
+            return "Wrong password";
+        }
+
     }
 
     @Override
     public double calculateInterest(int years) {
-        return 0;
+
+        return (balance+years+rateOfInterest)/100;
     }
 }
